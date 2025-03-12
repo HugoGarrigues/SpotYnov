c281927545e7a1cddc246ef902aadb80a87db52a
Garrigues <hugo.garrigues@ynov.com>
2025-03-12 15:32:49 +0100

    feat(addDaisyUi): Add daisyUI and tailwind for style

--- spotify-auth/src/components/Menu/Menu.tsx
+++ spotify-auth/src/components/Menu/Menu.tsx
@@ -1,26 +1,31 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import "./Menu.css";
 
 const Menu = () => {
-
     const token = localStorage.getItem("token");
+
     return (
-        <nav className="p-4 bg-gray-800 text-white flex justify-between">
-            <h1 className="text-xl font-bold">SpotYnov</h1>
-            <div>
+        <nav className="menu bg-base-100 rounded-box p-4 w-auto h-screen m-5 mb-5">
+            {/* Logo / Titre */}
+            <h1 className="text-xl font-bold text-center mb-8">SpotYnov</h1>
+
+            {/* Liens de navigation */}
+            <div className="flex flex-col space-y-4">
                 {token ? (
                     <>
-                        <Link to="/groups" className="mr-4">Groupes</Link>
-                        <Link to="/profile">Profil</Link>
+                        <Link to="/groups" className="hover:bg-gray-700 p-2 rounded-md transition duration-300">Groupes</Link>
+                        <Link to="/profile" className="hover:bg-gray-700 p-2 rounded-md transition duration-300">Profil</Link>
                     </>
                 ) : (
                     <>
-                        <Link to="/login" className="mr-4">Connexion</Link>
-                        <Link to="/register">Inscription</Link>
+                        <Link to="/login" className="hover:bg-gray-700 p-2 rounded-md transition duration-300">Connexion</Link>
+                        <Link to="/register" className="hover:bg-gray-700 p-2 rounded-md transition duration-300">Inscription</Link>
                     </>
                 )}
             </div>
         </nav>
+
+        
     );
 };
 
